87fc034b21ebd79b99d5b786e210ccf71c42ad69
Kharchenko_Niks <veronikkharchenko9@gmail.com>
2026-06-22 13:02:48 +0200

    Create retro timer window

--- src/components/TimerWindow/TimerW.tsx
+++ src/components/TimerWindow/TimerW.tsx
@@ -1,9 +1,20 @@
+import "../../styles/window.css"
+
 function TimerW(){
-    return(
-        <div className="window">
-            <h2>Timer.exe</h2>
-        </div>
-    );
+  return (
+    <div className="window">
+      <div className="window-header">
+        <span>⏰ Timer.exe</span>
+        <span>□ ✕</span>
+      </div>
+
+      <div className="window-body">
+        <h2>25:00</h2>
+
+        <button>Start</button>
+      </div>
+    </div>
+  );
 }
 
 export default TimerW;
